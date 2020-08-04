23e1765ec59beeff30962c9aa8602f4577b28e32
Paul Houghton <paul.houghton@gmail.com>
2020-08-04 07:44:01 +0100

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Paul Houghton on 04/08/2020.
+//  Copyright © 2020 Paul Houghton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
